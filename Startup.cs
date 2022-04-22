366e6664dcfe2ad15a9351c141663df17335e253
Mohssin Abihilal <54672371+mohssin19@users.noreply.github.com>
2022-04-22 03:13:48 +0100

    Update proj for ms entitry core to work. Created Dbcontext and seeded data

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cinema_ECommerce.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,9 @@ namespace Cinema_ECommerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DbContext config
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));  
+
             services.AddControllersWithViews();
         }
 
@@ -52,6 +57,9 @@ namespace Cinema_ECommerce
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //seed db
+            AppDbInitializer.Seed(app);
         }
     }
 }
